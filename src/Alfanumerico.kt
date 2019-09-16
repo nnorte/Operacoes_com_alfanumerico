1b8eacd073377de97a469982140af796a3999a6e
nnorte <nnorte@ucm.ac.mz>
2019-09-16 23:40:50 +0200

    Initial commit

---
+++ src/Alfanumerico.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+
+    //Determina o comprimento de uma cadeia de caracteres
+    println("Moçambique".length)
+    // Concatena duas cadeias de caracteres
+    println("Pemba é uma cidade moçambicana"+ ", sede de município e distrito, e capital da província de Cabo Delgado.")
+    //Converte uma cadeia de caracteres em letras maiusculas
+    println("Cidade de Pemba".toUpperCase())
+
+}
